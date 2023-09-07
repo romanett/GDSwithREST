ff7f90ec81a8964f24f37cd2c908767f8c065457
Roman Ettlinger <roman.ettlinger@siemens.com>
2023-09-07 11:08:06 +0200

    add GDS Server to project

---
+++ MinAPI/GdsDatabase.cs
@@ -0,0 +1,40 @@
+﻿using Opc.Ua;
+using Opc.Ua.Gds;
+using Opc.Ua.Gds.Server;
+using Opc.Ua.Gds.Server.Database;
+
+namespace MinAPI
+{
+    public class GdsDatabase : ApplicationsDatabaseBase, ICertificateRequest
+    {
+        public void AcceptRequest(NodeId requestId, byte[] certificate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ApproveRequest(NodeId requestId, bool isRejected)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CertificateRequestState FinishRequest(NodeId applicationId, NodeId requestId, out string certificateGroupId, out string certificateTypeId, out byte[] signedCertificate, out byte[] privateKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CertificateRequestState ReadRequest(NodeId applicationId, NodeId requestId, out string certificateGroupId, out string certificateTypeId, out byte[] certificateRequest, out string subjectName, out string[] domainNames, out string privateKeyFormat, out string privateKeyPassword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public NodeId StartNewKeyPairRequest(NodeId applicationId, string certificateGroupId, string certificateTypeId, string subjectName, string[] domainNames, string privateKeyFormat, string privateKeyPassword, string authorityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public NodeId StartSigningRequest(NodeId applicationId, string certificateGroupId, string certificateTypeId, byte[] certificateRequest, string authorityId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- MinAPI/Program.cs
+++ MinAPI/Program.cs
@@ -1,5 +1,38 @@
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using MinAPI;
+using Opc.Ua.Configuration;
+using Opc.Ua;
+using Opc.Ua.Gds.Server;
+using Microsoft.AspNetCore.Hosting.Server;
+
+ApplicationInstance application = new ApplicationInstance
+{
+    ApplicationName = "Global Discovery Server",
+    ApplicationType = ApplicationType.Server,
+    ConfigSectionName = "Opc.Ua.GlobalDiscoveryServer"
+};
+
+// load the application configuration.
+application.LoadApplicationConfiguration(false).Wait();
+
+// check the application certificate.
+application.CheckApplicationInstanceCertificate(false, 0).Wait();
+
+var database = new GdsDatabase();
+var gdsServer = new GlobalDiscoverySampleServer(
+        database,
+        database,
+        new CertificateGroup()
+       );
+application.Start(gdsServer).Wait();
+
+
+var endpoints = application.Server.GetEndpoints().Select(e => e.EndpointUrl).Distinct();
+foreach (var endpoint in endpoints)
+{
+    Console.WriteLine(endpoint);
+}
 
 var builder = WebApplication.CreateBuilder(args);
 
